a450ab6b49fde7f2200eaf38fdfdf9a547e76595
Wörrlein, Andreas <Andreas.Woerrlein@tecalliance.net>
2018-06-12 09:46:32 +0200

    main function kürzen

--- src/Cramersche.java
+++ src/Cramersche.java
@@ -1,20 +1,29 @@
 import java.io.*;
-public class Cramersche implements ISolver
+public class Cramersche 
 {
     public static void main(String args[]) throws IOException
     {
-    	
-    	
-        BufferedReader BR=new BufferedReader(new InputStreamReader (System.in));
+    	wdeingabeMatrix();
+        
+    }
+
+	
+	public static void wdeingabeMatrix() {
+		BufferedReader BR=new BufferedReader(new InputStreamReader (System.in));
         int Number[][]=new int[3][4];
         int i,j;
-        String m;
+        String m = null;
         System.out.println("Enter Elements for Matrix 3x3 :");
         for(i=0;i<=2;i++)
         {
             for(j=0;j<=3;j++)
             {
-                m=BR.readLine();
+                try {
+					m=BR.readLine();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
                 Number[i][j]=Integer.parseInt(m);
             }
         }
@@ -28,11 +37,6 @@ public class Cramersche implements ISolver
             }
             System.out.println();
         }
-    }
-
-	@Override
-	public void eingabeMatrix() {
-		// TODO Auto-generated method stub
 		
 	}
 }
