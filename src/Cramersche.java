e3d12a3db483ad1113496bfbe1ed41278b1694be
Wörrlein, Andreas <Andreas.Woerrlein@tecalliance.net>
2018-06-25 22:30:21 +0200

    Interface richtig ?

--- src/Cramersche.java
+++ src/Cramersche.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.Arrays;
-public class Cramersche
+public class Cramersche implements ISolver
 {
    
     public static void main(String args[]) throws IOException
--- src/ISolver.java
+++ src/ISolver.java
@@ -1,5 +1,19 @@
 
 public interface ISolver {
-
-	public void eingabeMatrix();
+	
+	double det=0;
+	double ergebnis[] = new double[3];
+	double Number[][]=new double[3][4];
+	
+	public static double[][] eingabeMatrix() {
+		return Number;
+	}
+	
+	public static double[] Calculate(double[][] Number) {
+		return ergebnis;
+	}
+	
+	public static double CalculateDet(double[][] a) {
+		return det;
+	}
 }
